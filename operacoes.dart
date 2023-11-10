ade541219523f2b013d67ea20a0086a6658ea7df
Antonio-Secchin <101224741+Antonio-Secchin@users.noreply.github.com>
2023-11-09 21:40:53 -0300

    Add files via upload

---
+++ operacoes.dart
@@ -0,0 +1,9 @@
+void main(){
+  int number = 10;
+  int number2 = 10;
+  print(number + number2);
+  print(number - number2);
+  print((number * number2));
+  print(number / number2);
+  print(number%2);
+}
---
+++ tiposDeDados.dart
@@ -0,0 +1,24 @@
+void main(){
+  int number = 10;
+  print("Tipo inteiro");
+  print(number);
+  
+  double flutuante = 10.0;
+  print("Tipo double");
+  print(flutuante);
+  
+  String texto = "Algum texto aqui";
+  print("Tipo String");
+  print(texto);
+  
+  bool verdadeiro = true;
+  print("Tipo Bool");
+  print(verdadeiro);
+  
+  dynamic dinamico = 10;
+  print("Tipo Dinamico");
+  print(dinamico);
+  dinamico = "Algum Texto 2";
+  print("Tipo Dinamico");
+  print(dinamico);
+}
